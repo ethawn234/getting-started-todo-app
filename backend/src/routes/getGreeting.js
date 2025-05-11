33df00df6211aaac8eb746b522860dc30b5e242b
ethawn <ethawn234@gmail.com>
2025-05-11 14:02:56 -0500

    add docker demo

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,7 +1,13 @@
-const GREETING = 'Hello world!';
+const GREETINGS = [
+    "You're Whalecome!",
+    "All hands on deck!",
+    "Charting the course ahead!",
+    "I am farmer, hear me plow!",
+    "DevOps, here I come!"
+];
 
 module.exports = async (req, res) => {
     res.send({
-        greeting: GREETING,
+        greeting: GREETINGS[ Math.floor( Math.random() * GREETINGS.length )],
     });
 };
